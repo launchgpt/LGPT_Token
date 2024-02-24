3b4a1a296d39cf49018b3f905fe3d45a4ea0f177
launchgpt <158581006+launchgpt@users.noreply.github.com>
2024-02-24 18:05:57 +0800

    Update ChainGPT.sol

--- ChainGPT.sol
+++ ChainGPT.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ChainGPT is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("LaunchGPT", "LGPT") {
+contract LaunchGPT is ERC20, ERC20Burnable, Ownable {
+    constructor() Ownable(msg.sender) ERC20("LaunchGPT", "LGPT") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
