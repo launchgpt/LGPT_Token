4b62cf5bf55d686660452f76e810f60c2553439d
launchgpt <158581006+launchgpt@users.noreply.github.com>
2024-02-24 16:19:04 +0800

    Update ChainGPT.sol

--- ChainGPT.sol
+++ ChainGPT.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ChainGPT is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("ChainGPT", "CGPT") {
+    constructor() ERC20("LaunchGPT", "LGPT") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
